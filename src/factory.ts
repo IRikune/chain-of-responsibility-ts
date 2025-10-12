ddc1e3186626ba2b3f780ddc6e9ca7a3032820c3
CodeArmer <geanmotas@gmail.com>
2025-10-11 23:22:36 -0400

    fix: fixed types in factory

--- src/factory.ts
+++ src/factory.ts
@@ -4,16 +4,18 @@ import { Context } from "@/context.ts";
 
 export function createStep<T extends Record<PropertyKey, unknown>>(
   step: Step<T>,
-) {
+): Step<T> {
   return step;
 }
 
 export function createChain<
   T extends Record<PropertyKey, unknown> = Record<PropertyKey, never>,
->() {
+>(): Chain<T> {
   return new Chain<T>();
 }
 
-export function createContext<T extends Record<PropertyKey, unknown>>() {
+export function createContext<
+  T extends Record<PropertyKey, unknown>,
+>(): Context<T> {
   return new Context<T>();
 }
